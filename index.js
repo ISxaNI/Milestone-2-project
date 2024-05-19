1f515770cf2e06eb1269a2c4975666463e2cd5ee
ISxaNI <jasonliu99999@yahoo.com>
2024-05-19 13:40:51 -0700

    Adding files to empty folders

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
 
-
+// Express settings from rest-rant
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.send('i dont like panos')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).render('error404')
 })
 
 app.listen(3000)
